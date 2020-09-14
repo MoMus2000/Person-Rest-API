01f569fbee0785b0e9adb994c5bf7669da424696
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2020-09-14 22:37:07 +0500

    Add files via upload

---
+++ full-flash-api/src/test/java/io/javabrains/FullFlashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullFlashApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
